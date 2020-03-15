23dde4379e346573a3b361ae1a6958e07e0dc0e4
HuangYH1995 <huang.950724@gmail.com>
2020-03-15 22:24:05 +0800

    Update fib.cpp
    費氏數列(including recursive and iterative versions)

--- fib.cpp
+++ fib.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int fib(int);
+int fib_recursive(int);
+int fib_iterative(int);
 
 int main(void){
+
 	for(int i=0; i<10; i++){
-		cout << i << "  " << fib(i) << endl;	
+		cout << i << "  " << fib_recursive(i) << "  " << fib_iterative(i) << endl;	
 	}
 	return 0;
 }
 
-int fib(int n){
+int fib_recursive(int n){
 	if(n==0) return 0;
 	else if(n==1) return 1;
-	else return fib(n-1)+fib(n-2);
+	else return fib_recursive(n-1)+fib_recursive(n-2);
+}
+
+int fib_iterative(int n){
+	if(n==0) return 0;
+	else if(n==1) return 1;
+	else{
+		int a=0, b=1, c=0;
+		for(int i=2; i<=n; i++){
+			c = a+b;
+			a = b;
+			b = c;
+		}
+		return c;
+	}
 }
